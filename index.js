7d0dad6397e33e16847a7087d9500addbcccf720
Trang Le <trang.thule@zoho.com>
2021-12-09 15:40:18 +0700

    feat(script): construct graphql query

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Does this run?")
+console.log(process.env)
+const now = new Date()
+const nowIso = now.toISOString()
+
+const query = '{\
+  search(first: 2, type:ISSUE, query: "") {\
+    issueCount\
+  }\
+}'
