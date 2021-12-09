2cc48a07bc904555c51489a9f519687e898541bd
Trang Le <trang.thule@zoho.com>
2021-12-09 17:03:24 +0700

    feat(script): log repo, author and merged time

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ let token = process.env.INPUT_REPO_TOKEN
 if (!token) {
   return
 } else {
+  const repo = process.env.GITHUB_REPOSITORY
   const rawData = fs.readFileSync(process.env.GITHUB_EVENT_PATH)
   const pr = JSON.parse(rawData)
-  console.log(pr)
+  // console.log(pr)
+  const author = pr.user.login
+  const mergedAt = pr.merged_at
+  console.log({ repo })
+  console.log({ author })
+  console.log({ mergedAt })
 }
 // const now = new Date()
 // const nowIso = now.toISOString()
