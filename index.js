937e219e23160b757a74b53f8c4b589101ca1088
Trang Le <trang.thule@zoho.com>
2021-12-09 16:45:52 +0700

    feat(script): parse event JSON file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
 let githubEvent = process.env.EVENT
 githubEvent = JSON.parse(githubEvent)
-console.log(process.env)
+// console.log(process.env)
 let token = process.env.INPUT_REPO_TOKEN
 if (!token) {
   return
 } else {
   const rawData = fs.readFileSync(process.env.GITHUB_EVENT_PATH)
-  console.log(rawData)
+  const pr = JSON.parse(rawData)
+  console.log(pr)
 }
 // const now = new Date()
 // const nowIso = now.toISOString()
