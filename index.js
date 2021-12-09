a14708a86e543ff30629dd11571c4810b6be7484
Trang Le <trang.thule@zoho.com>
2021-12-09 16:37:59 +0700

    debug: read event JSON file

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const fs = require('fs')
 let githubEvent = process.env.EVENT
 githubEvent = JSON.parse(githubEvent)
 console.log(process.env)
-console.log(githubEvent)
-if (process.env.INPUT_REPO_TOKEN) {
-  console.log("repo token exists")
+let token = process.env.INPUT_REPO_TOKEN
+if (!token) {
+  return
+} else {
+  const rawData = fs.readFileSync(process.env.GITHUB_EVENT_PATH)
+  console.log(rawData)
 }
-const now = new Date()
-const nowIso = now.toISOString()
+// const now = new Date()
+// const nowIso = now.toISOString()
 
-const query = '{\
-  search(first: 2, type:ISSUE, query: "") {\
-    issueCount\
-  }\
-}'
+// const query = '{\
+//   search(first: 2, type:ISSUE, query: "") {\
+//     issueCount\
+//   }\
+// }'
